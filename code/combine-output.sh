2eff0a7629ec27a5f29cc875a6d55231491922ad
James Pooley <james.pooley@childmind.org>
2016-05-03 16:41:42 -0400

    Add comments

--- code/combine-output.sh
+++ code/combine-output.sh
@@ -10,6 +10,8 @@ DLOAD_BASE=s3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1
 # Download Preprocessed ABIDE Data from S3 Bucket
 
 measures=(GrayVol ThickAvg)
+
+# awk sorcery to get subject IDs from phenotypic information spreadsheet
 subj_list=$(awk -F "\"*,\"*" 'FNR>1{print $7}' ${DATA_DIR}/Phenotypic_V1_0b_preprocessed1.csv)
 
 # if [ ! -d $DATA_DIR/${subj_id}/ ]; then
